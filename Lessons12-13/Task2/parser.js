e64ba87216d1f40e3b7ce5fd6e320d542d579174
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-12-16 16:22:23 +0300

    Task 13: added buttons, len finding func between two dots

---
+++ Lessons12-13/Task2/parser.js
@@ -0,0 +1,92 @@
+function parse_data() {
+    let filtered_data = []
+
+    let json_data = JSON.parse(JSON.stringify(data));
+
+    for (const i in json_data) {
+        var territorialDivisionOfProperty = {
+            id: json_data[i].id,
+            full_name: json_data[i].FullName,
+            short_name: json_data[i].ShortName,
+            adm_area: json_data[i].AdmArea,
+            district: json_data[i].District,
+            phone: json_data[i].Phone,
+            address: json_data[i].Address,
+            chief_name: json_data[i].ChiefName,
+            geo_data: json_data[i].geoData,
+            geodata_center: json_data[i].geodata_center
+        };
+
+        filtered_data.push(territorialDivisionOfProperty)
+    }
+    let data_out = JSON.stringify(filtered_data)
+    return filtered_data
+}
+
+function printCanvas(coords) {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    // coords.forEach(xy => ctx.fillRect(xy[0], xy[1], 2, 2))
+    ctx.fillRect(coords[0], coords[1], 2, 2)
+}
+
+function getStatistic(data) {
+    let neigh = []
+    data.forEach(element => neigh.push([element.adm_area, element.district, element.address]))
+    return neigh
+}
+
+function generateRandomNumbers(max, len) {
+    let listOfNums = []
+    for (let i = 0; i < len; i++) {
+        listOfNums.push([Math.floor(Math.random() * max), Math.floor(Math.random() * max)]);
+    }
+    return listOfNums
+}
+
+function clearCanvas() {
+    const canvas = document.getElementById("myCanvas");
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function printParsedData() {
+    clearCanvas();
+    parse_data().forEach(el => printCanvas(el.geo_data.coordinates))
+}
+
+function printGeneratedNumbers(event) {
+    clearCanvas();
+    generateRandomNumbers(event.currentTarget.max, event.currentTarget.len).forEach(element => printCanvas(element));
+    taskFunc(event.currentTarget.numbers)
+}
+
+function taskFunc(x) {
+    // console.log(findLenBtwDots(x[0], x[1]))
+    for (let j = 0; j < x.length; j++) {
+        for (let i = 0; i < x.length; i++) {
+            console.log(j, i)
+        }
+    }
+    // console.log(x[0])
+    // console.log(x[1])
+}
+
+function findLenBtwDots(dot1, dot2) {
+    return Math.sqrt(Math.pow(dot1[0] - dot2[0], 2) + Math.pow(dot1[1] - dot2[1], 2))
+}
+
+function main() {
+    // console.log(parse_data())
+    // console.log(getStatistic(parse_data()))
+
+    document.getElementById("json_button").addEventListener("click", printParsedData, false);
+
+    const genButton = document.getElementById("generated_button")
+    genButton.addEventListener("click", printGeneratedNumbers, false);
+    genButton.numbers = generateRandomNumbers(500, 100);
+    genButton.max = 500;
+    genButton.len = 100;
+
+}
---
+++ Lessons12-13/Task2/server.js
@@ -0,0 +1,54 @@
+const fs = require("fs")
+console.log("Чтение файла")
+let data = fs.readFileSync("./Lessons12-13/Task1/data/raw_data.js", "utf8");
+
+let data_len = data.length;
+let filtered_data = []
+
+let json_data = JSON.parse(data);
+
+for (const i in json_data) {
+    var territorialDivisionOfProperty = {
+        id: json_data[i].id,
+        full_name: json_data[i].FullName,
+        short_name: json_data[i].ShortName,
+        adm_area: json_data[i].AdmArea,
+        district: json_data[i].District,
+        phone: json_data[i].Phone,
+        address: json_data[i].Address,
+        chief_name: json_data[i].ChiefName,
+        geo_data: json_data[i].geoData,
+        geodata_center: json_data[i].geodata_center
+    };
+
+    filtered_data.push(territorialDivisionOfProperty)
+}
+
+try {
+    fs.writeFileSync("./Lessons12-13/Task1/data/data_out.js", JSON.stringify(filtered_data))
+    console.log("Запись в файл отфильтрованных данных")
+} catch (err) {
+    console.error(err)
+}
+
+
+
+const axios = require('axios');
+
+// async function search(str) {
+//   const geocoderUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+//     encodeURIComponent(str) +
+//     '.json?access_token=' +
+//     'pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g';
+//
+//   const res = await axios.get(geocoderUrl).then(res => res.data);
+//   const point = res.features[0].geometry;
+//
+//   return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/' +
+//     'pin-l-1+333(' + point.coordinates[0] + ',' + point.coordinates[1] + ')/' +
+//     point.coordinates[0] + ',' + point.coordinates[1] +
+//     ',14.25,0,0/600x600/' +
+//     '?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g';
+// }
+
+search('429 Lenox Ave, Miami Beach').then(res => console.log(res));

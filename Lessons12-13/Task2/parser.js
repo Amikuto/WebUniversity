f8474b47d51ec207ae9700649b38c7a7ba0ab96a
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-19 22:29:35 +0300

    Task 13: personal task 13 done

--- Lessons12-13/Task2/parser.js
+++ Lessons12-13/Task2/parser.js
@@ -58,34 +58,61 @@ function printParsedData() {
 
 function printGeneratedNumbers(event) {
     clearCanvas();
-    generateRandomNumbers(event.currentTarget.max, event.currentTarget.len).forEach(element => printCanvas(element));
-    taskFunc(event.currentTarget.numbers)
+    event.currentTarget.numbers.forEach(element => printCanvas(element));
+    taskFunc(event.currentTarget.numbers);
 }
 
 function taskFunc(x) {
-    // console.log(findLenBtwDots(x[0], x[1]))
+    let list1 = []
     for (let j = 0; j < x.length; j++) {
+        let list2 = [];
         for (let i = 0; i < x.length; i++) {
-            console.log(j, i)
+            if (i === j) {
+                continue;
+            }
+            list2.push(findLenBtwDots(x[j], x[i]));
         }
+        list1.push(arrayMin(list2));
     }
-    // console.log(x[0])
-    // console.log(x[1])
+
+    const indexMinor = list1.indexOf(Math.min(...list1));
+    console.log(x[indexMinor]);
+    colorDot(x[indexMinor]);
+}
+
+function arrayMin(arr) {
+    return arr.reduce(function (p, v) {
+        return (p < v ? p : v);
+    });
+}
+
+function arrayMax(arr) {
+    return arr.reduce(function (p, v) {
+        return (p > v ? p : v);
+    });
 }
 
 function findLenBtwDots(dot1, dot2) {
     return Math.sqrt(Math.pow(dot1[0] - dot2[0], 2) + Math.pow(dot1[1] - dot2[1], 2))
 }
 
-function main() {
-    // console.log(parse_data())
-    // console.log(getStatistic(parse_data()))
+function colorDot(coords) {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
 
+    ctx.fillStyle = "Red";
+    ctx.fillRect(coords[0], coords[1], 4, 4)
+    ctx.fillStyle = "Black";
+}
+
+function main() {
     document.getElementById("json_button").addEventListener("click", printParsedData, false);
+
+    const generatedNumbers = generateRandomNumbers(500, 100);
 
     const genButton = document.getElementById("generated_button")
     genButton.addEventListener("click", printGeneratedNumbers, false);
-    genButton.numbers = generateRandomNumbers(500, 100);
+    genButton.numbers = generatedNumbers
     genButton.max = 500;
     genButton.len = 100;
 

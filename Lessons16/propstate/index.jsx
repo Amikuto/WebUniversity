0719880eb4bf6ba35fb72c94334515b1818a268e
Damir Kurtaev <damir.kurtaev@yandex.ru>
2022-02-22 16:11:53 +0300

    Task 16: "node.js" and "Props, State и состояния" методички

---
+++ Lessons16/propstate/index.jsx
@@ -0,0 +1,42 @@
+class Hello extends React.Component {
+    render() {
+        return <div>
+            <p>Имя: {this.props.name}</p>
+            <p>Возраст: {this.props.age}</p>
+        </div>;
+    }
+}
+
+ReactDOM.render(
+    <Hello name="Damir" age="20"/>,
+    document.getElementById("app")
+)
+
+
+class ClickButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {class: "off", label: "Click!"};
+
+        this.press = this.press.bind(this);
+    }
+
+    press() {
+        let className = (this.state.class === "off") ? "on" : "off";
+        this.setState({class: className});
+    }
+
+    render() {
+        return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>;
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Hello name="Damir" age="20"/>
+        <ClickButton/>
+    </div>,
+document.getElementById("app")
+)
+

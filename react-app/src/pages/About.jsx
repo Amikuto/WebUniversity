0b45a321a73f6d5e162cb6f36c0a7103f7bc6412
Damir Kurtaev <damir.kurtaev@yandex.ru>
2022-04-10 17:00:00 +0300

    TypeScript added, about page added surname, old css fixes, react readme deleted

--- react-app/src/pages/About.jsx
+++ react-app/src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>Автор: Дамир ПИ19-4</h1>
+            <h1>Автор: Куртаев Дамир ПИ19-4</h1>
         </div>
     );
 };

e4a9f4dd8ae3b4e3bf4c3494499957f63c1b028d
Damir Kurtaev <damir.kurtaev@yandex.ru>
2022-04-11 21:48:03 +0300

    JS4.15 Компоненты, основанные на классах done

--- react-app/src/pages/FuncClass.jsx
+++ react-app/src/pages/FuncClass.jsx
@@ -3,9 +3,39 @@ import React from 'react';
 const FuncClass = () => {
     return (
         <div>
-
+            <Comment author={{avatarUrl: require("../static/images/9sH_WJPWW8k.jpg"), name: "Damir"}} text={"Some simple text"} date={(new Date).toLocaleString()}/>
         </div>
     );
 };
 
 export default FuncClass;
+
+
+class Comment extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"Comment"}>
+                <div className={"UserInfo"}>
+                    <img className={"Avatar"}
+                         style={{width: "600px", height: "600px"}}
+                    src={this.props.author.avatarUrl}
+                    alt={this.props.author.name}/>
+                    <div className={"User-Info-name"}>
+                        {this.props.author.name}
+                    </div>
+                </div>
+                <div className={"Comment-text"}>
+                    {this.props.text}
+                </div>
+                <div className={"Comment-date"}>
+                    {this.props.date}
+                </div>
+            </div>
+            )
+    }
+}
+
